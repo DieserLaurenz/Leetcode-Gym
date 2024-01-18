ce6bda1c3ee1785484ac37d92ed70cb8d4f9e5e6
Laurenz Gilbert <@gilbert@uni-potsdam.de>
2024-01-18 15:38:24 +0100

    Rewrote leetcode_gym to handle caching. Also waits no until time limit is over.

--- chatgptapi_new.py
+++ chatgptapi_new.py
@@ -1,11 +1,16 @@
+import json
 import time
 
 from re_gpt import SyncChatGPT
 
+from gptplus import ChatGPT
+
+from gpt4_openai import GPT4OpenAI
+
 session_token = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..n_nyr6igvLpysoc6.LaSsRPjgHlargangaa-BbKLAWVvhR_tnHAdJDTgfDa2kGYjXC5wLS556qk-KjtPeGBnjyrml11p8juV-WTS1zOAFb1qI6O1PzI_3yzFEo-1sESRL7YLpBqtfEUs-V-AtL5icJ3fgg-hmBETeAQiiTn3D6SSOK-a8TVXooY7nUipwSsO5WOK3R9FhlyEI8c13Vx6Rmi73uLJtE_1U8xc5cV2nyHgv_d35oitViAEw5u8stK2eGNM7hWpiNwpYjQf66RP4fj7682kyPZdx2uaRQqHoGSE-J1MnLR_L1c8P8SzUJNfcaF-y3v_GKsPP3G3wUnHZcAvVCUuLMz2kzV98Fy--BQvURmoiRi-3zO03x_pY0rQaidHjDNmVBzewPsz9tUkUAULj98sLJtiSF6kMH_ryuvAEL5KX5vkA8mLNlPpg25f4FXOw6xacUqTozA-xGL6eR1Eb0QT16mfairbZW-XHVMgB0YxcjEXpOA5r_zlUKJk7Nbzc2RSkj3Mm71_wjTi1IkSvmA3erOE246Vy_MDblrQqMIJpISujwAYzVUsdv_ndXQj8hzzj2JC-SwxJZDxFMoXdYo9d_0c00yQgtZrZjMo2qmxvNM98HCccYOCmj5nBQJHY3VmdaMlqShR8rXK73uBF7tT7HBSXODCJkrMKYpSDQ_VJVjjCq8aWYQ-5fjNm7riPn5R8pGI4EKd6XVtPpscxK0i4Ho54KX2TLhI_FsdX3wgzfwpfiaOSJIkQGyV8L9Qi50aWFEOY61N54sFPlE2CxJ8N9aNAJ96ZyoJaTFrFfnTIspKxJECwRNNFI14iyeF0yZ_X6F2k1LmOMqcPH0aTY7C1Eo8P5uf4X9P6wFj1dJNPnZQ0sY1XojFMkwtrMTtsVgxKWcX7f4f5ad8vp04vzqfUFO_CZSoEUGgAQynda66JqGG1bMngZ5zFJBLwW2bueAFxUSyFDtie_Tf2z5tA2VcVaglIqMPVSv8td3QEDFq6wGwCVs2Bf75QSi-hZBM20f499zk_22a3llqkTmDWqmbhgimlLNbGudLklw1YIN2L-g9wn0nFcaorCPVeAklZD6big8rpXo1QapfBhpux-S2Hiccs3M7vPpd4sMwx2Pjxi_vw-e9oTlKduvmURr2PvEuuylGjhe-cKUjazcvz_jQxrXv3692RnIQLT-l0NRXAo7cu6E4HdGj6qpA8xI5ew97d7WHl02u2A9A20IuGVJHB63u4veVZ5lhOkFuvWezwXvaXgGWk6Q8zx3XmJnE3i1AznbnGom_lRIGng_obQ6Pc07-wOcyJ6H1dfKomUG7gjAVctkXDUYGgxhq_E21OXYf0Pu1oBnewjMjRtJE63ggYfH1DxC_O9nztODlj4vUNsqhNXKySNlxWRldxusuO-0AS0FlFcdnW2fcy9A6YiHtw4s_WO-l70drx8HgER6lOnDI3hMEbyO4BSNcr_k4SICT9bsAjVtqWk4okUag5dkL2kG6VXbNcDGu4C2irDrrdX52SPiO-jBg4BxUt_OHO3QRWCc6IDE3HdudmYariFobuc9eeyS6qCUB1lqksbWtIV7KKyH72Wbih22igbcTkRGxK_XN4ty5shmD_A9IMywDBGhXFNaSmpMaXnp7qG_mwYO16iCCbRE4pNveZzu6oNmgrsz_IvJhuY1W3hYn3MbydjzL__h8fUjVpPW6n5SQh2ztsSbd7iD_Mo1OPmK4xUZ6CxKPgjBToWqDp2iBHxheFvIK616rYSwRbzqCZ-C7eTVU1kQx3hi5sPRST0IjYGmVGu-NOBw40P9fKxmznwkQGcc5txR_meFRQxM9cJm4PuN07DGWljJOTgpMag9ql7J6p8WOWM6EBD-YdSVlpIzrktKE7GuMjb-yRzoCKU-8wRUquLicS5EE9IpSaUqGElpmVfv3nVUj4pWnT-7thNQ2_cHfwGnYbkBS_TSTo_WwRuw-6E3tl9Kxp0GRAuS59IPOa5PEXKcGWzMd_LfiZJ5YO53ngsX094vfxksnJvRLNe6a5HFgvYUnA6o3KzbOJDpEZQd-e4Ywq8flNVVp4to21XwNzIUf_61lDVstsdtxV_rb9kCc1OA2Qt4uxfMhLtR6Gj4H6OTSwVLda7wDMn7LArdVSUs9z0anNXW7mO_eU72SS5is3vH3fppFR5VuYG7cdAT9Nb_p-pWo6nBLmGJOuRUaVnin--SlRC5Y_qL1I4yb-R-wUpj2K8p2xfX5ywowSIa61n8SMys0PjQByN4NpW8N9j5qKhdotNSEgZILz_Bq_3SosZjxcIYRPlwSdp9wvQyqS9HyufrkoBakCZ0Y89Gy8LDYixNcV4vUKrO-zSow5G_FR6E2NQ0l1cP5AkMGhUmo2qG1-ie2LPMlJoJ9pfEMuqzoMtpN7VxoH9y8EUETXeQpLKrsrEnXYBhqGVVB54Ta0Tlx3mqV9_VJAv6YSiPxja0zhBsPluvI0e5APLz3PWr1Mpe-ZEl4Y_kXYaDBF3UZI6EdekhIs5PPtm1f9Ief2gHk-QEiK2744RmjBYNKiwuz7pf1JMY7QvybRtMn_ZWSmevcdW4TFcy0SBRwflSqg9AWGQ15tYUv3JcQQxMHSbA2eggJdMM8-xk9EwlWIl_6lPY-xc8Mwat-qxbhr7kHMnK_EaCAuavQMsF3I0P5H8NCV8-22HosGLNvgzMWmdB-zITzzlcxwNPHTqjh-8l6ZF_2zFqHkttj0XlF_ovvmhO8rB6IcW2y3Tkh7RHt9QajpSZx-ilBWekN-cm4877LypFsl7G17HtfcPNZJgwwOStwR6G2OUPwXs2SsougnMbqumidXO-mes7WsN4f5PQ6Y0AHRuK4wkus4S0jxO7TMeQB_7l3jasTmjUrMHxnjhhmFRE0xweZk9R5xKBy9vKuC43-Q0E4v7kLb0dCc3B57ESfiIxmJ2EdspR194lj_U8c.HOBidOGJPolLoKbdcXqleg"
 
 
-def send_message(prompt, conversation_id=None):
+def send_message_with_SyncChatGPT(prompt, conversation_id=None):
     with SyncChatGPT(session_token=session_token) as chatgpt:
 
         if conversation_id:
@@ -15,16 +20,44 @@ def send_message(prompt, conversation_id=None):
 
         response = ""
 
-        while(True):
+        while (True):
             try:
                 for message_chunk in conversation.chat(prompt):
                     response += message_chunk["content"]
             except Exception as e:
                 print(e)
-                time.sleep(5)
+
+                exception = str(e)
+
+                # Finde den Start des JSON-Strings
+                start = exception.find('{')
+                # Finde das Ende des JSON-Strings
+                end = exception.rfind('}') + 1
+
+                # Extrahiere den JSON-Teil des Strings
+                json_part = exception[start:end]
+
+                # Parse den JSON-String
+                data = json.loads(json_part)
+
+                # Extrahiere die gewünschten Werte
+                message = data["detail"]["message"]
+                clears_in = data["detail"]["clears_in"]
+
+                print(message)
+                print("Sleeping for: ", clears_in)
+
+                time.sleep(clears_in)
                 continue
 
             break
 
-
         return response, conversation.conversation_id
+
+
+def send_message_with_gpt4openai():
+    # Token is the __Secure-next-auth.session-token from chat.openai.com
+    llm = GPT4OpenAI(token=session_token, headless=False, model='gpt-4')
+    # GPT3.5 will answer 8, while GPT4 should be smart enough to answer 10
+    response = llm('If there are 10 books in a room and I read 2, how many books are still in the room?')
+    print(response)
--- leetcode_gym.py
+++ leetcode_gym.py
@@ -1,15 +1,21 @@
-import os
 import json
+import os
 import re
+import shelve
 
-import leetcode_submitter
-import chatgptapi
 import chatgptapi_new
-import pyperclip
+import leetcode_submitter
+
 
 def read_json_file(file_path):
     with open(file_path, 'r') as json_file:
         return json.load(json_file)
+
+
+def save_response(response_directory, response_filename, submission_response):
+    with open(os.path.join(response_directory, response_filename), 'w') as file:
+        json.dump(submission_response, file, indent=4)
+
 
 def extract_code(answer):
     lang_slugs = ['cpp', 'java', 'python', 'python3', 'c', 'csharp', 'javascript', 'typescript', 'php', 'swift',
@@ -21,6 +27,65 @@ def extract_code(answer):
         if match:
             return match.group(1)
     return None
+
+
+def process_snippet(prompt, subfolder_path, question, snippet, attempt, conversation_id):
+    lang_slug = snippet['langSlug']
+    title_slug = question['titleSlug']
+    question_id = question['questionId']
+    cache_path = 'snippet_cache.db'
+    response_directory = os.path.join(subfolder_path, 'responses')
+    problem_url = f"https://leetcode.com/problems/{title_slug}"
+
+    if lang_slug in ('python', 'python3'):
+        print(f"Python. Skipping.")
+        return True, "", ""
+
+    if check_cache(cache_path, question_id, lang_slug):
+        print(f"Cache hit for {question_id} in {lang_slug}. Skipping.")
+        return True, "", ""
+    else:
+        print(f"Processing: {title_slug} in {lang_slug}.")
+
+    answer, conversation_id = chatgptapi_new.send_message_with_SyncChatGPT(prompt, conversation_id)
+    extracted_code = extract_code(answer)
+    print(extracted_code)
+
+    if not extracted_code:
+        print("Kein Code gefunden")
+        return False, "", ""
+
+    submission_response = leetcode_submitter.main(problem_url, question_id, lang_slug, extracted_code)
+
+    # Überprüfe, ob die Lösung akzeptiert wurde
+    if submission_response.get("status_msg") == 'Accepted':
+        with shelve.open(cache_path) as cache:
+            cache_key = f"{question_id}_{lang_slug}"
+            cache[cache_key] = submission_response
+
+        response_filename = f'response_{lang_slug}_{attempt}_success.json'
+        save_response(response_directory, response_filename, submission_response)
+        return True, "", ""
+    elif attempt == 3:
+        with shelve.open(cache_path) as cache:
+            cache_key = f"{question_id}_{lang_slug}"
+            cache[cache_key] = submission_response
+    else:
+        error_prompt = extract_info_and_generate_prompt(submission_response)
+        print(f"Fehler-Prompt für Versuch {attempt + 1}: {error_prompt}")
+
+        response_filename = f'response_{lang_slug}_{attempt}_failed.json'
+        save_response(response_directory, response_filename, submission_response)
+        return False, error_prompt, conversation_id
+
+
+def generate_prompt_content(question, snippet):
+    return (
+        f'Solve the following problem in {snippet["lang"]}. '
+        'Use the provided template as a starting point.\n\n'
+        f'Template:\n\n{snippet["code"]}\n\nProblem:\n\n{question["content"]}\n\n'
+    )
+
 
 def extract_info_and_generate_prompt(response):
     error_type = response['status_msg']
@@ -60,86 +125,39 @@ def extract_info_and_generate_prompt(response):
 
     return " ".join(prompt_parts)
 
+
+def check_cache(cache_path, question_id, lang_slug):
+    with shelve.open(cache_path) as cache:
+        return cache.get(f"{question_id}_{lang_slug}") is not None
+
+
+def process_question(json_file_path, subfolder_path):
+    question = read_json_file(json_file_path)
+
+    for snippet in question["codeSnippets"]:
+        attempt = 0
+        conversation_id = None
+        prompt = generate_prompt_content(question, snippet)
+        while attempt < 3:
+            is_success, error_prompt, conversation_id = process_snippet(prompt, subfolder_path, question, snippet,
+                                                                        attempt, conversation_id)
+            if is_success:
+                break  # Beende die Schleife, wenn die Lösung akzeptiert wurde
+            attempt += 1
+            prompt = error_prompt
+
+
 def process_subfolder(base_path, subfolder):
     subfolder_path = os.path.join(base_path, subfolder)
     json_file_path = os.path.join(subfolder_path, f'{subfolder}.json')
-    prompt_directory = os.path.join(subfolder_path, 'prompts')
-    response_directory = os.path.join(subfolder_path, 'responses')  # Added directory for responses
+    response_directory = os.path.join(subfolder_path, 'responses')
 
-    os.makedirs(prompt_directory, exist_ok=True)
-    os.makedirs(response_directory, exist_ok=True)  # Create the responses directory
+    os.makedirs(response_directory, exist_ok=True)
 
     if not os.path.exists(json_file_path):
         return
 
-    question = read_json_file(json_file_path)
-    title_slug = question['titleSlug']
-    question_id = question['questionId']
-    problem_url = f"https://leetcode.com/problems/{title_slug}"
-    print(f"Processing: {title_slug}")
-
-    for snippet in question["codeSnippets"]:
-        messages = []
-        conversation_id = None
-        response_counter = 1  # Initialize a counter for responses
-
-        lang = snippet['lang']
-        lang_slug = snippet['langSlug']
-        prompt_content = (
-            f'Solve the following problem in {lang}. '
-            'Use the provided template as a starting point.\n\n'
-            f'Template:\n\n{snippet["code"]}\n\nProblem:\n\n{question["content"]}\n\n'
-        )
-
-        print(f"Initial prompt: {prompt_content}")
-
-        #messages.append({'role': 'user', 'content': prompt_content})
-
-        for i in range(3):
-            #answer = chatgptapi.main(messages)
-            answer, conversation_id = chatgptapi_new.send_message(prompt_content, conversation_id)
-
-            print(f"ChatGPT Antwort: {answer}")
-
-            extracted_code = extract_code(answer)
-
-            if extracted_code:
-                print("Code erfolgreich extrahiert")
-            else:
-                print("Kein Code gefunden")
-
-            #messages.append({'role': 'assistant', 'content': answer})
-
-            submission_response = leetcode_submitter.main(problem_url, question_id, lang_slug, extracted_code)
-
-            # Determine the filename based on success or failure
-            response_status = "success" if submission_response["run_success"] and submission_response["status_msg"] == 'Accepted' else "failed"
-            response_filename = f'response_{lang}_{response_counter}_{response_status}.json'
-
-            # Increment the response counter
-            response_counter += 1
-
-            # Save the submission response
-            with open(os.path.join(response_directory, response_filename), 'w') as file:
-                json.dump(submission_response, file, indent=4)
-
-            if submission_response["run_success"] == True:
-                if submission_response["status_msg"] == 'Wrong Answer':
-                    error_prompt = extract_info_and_generate_prompt(submission_response)
-                elif submission_response["status_msg"] == 'Accepted':
-                    print("The solution is correct")
-                    break
-                else:
-                    print(f"Another error message: {submission_response['status_msg']}")
-                    exit()
-            else:
-                error_prompt = extract_info_and_generate_prompt(submission_response)
-
-            print("ChatGPT produced a wrong answer...")
-            print(f"Error prompt: {error_prompt}")
-
-            prompt_content = error_prompt
-
+    process_question(json_file_path, subfolder_path)
 
 
 def process_folders(base_path, folders):
@@ -153,5 +171,6 @@ def process_folders(base_path, folders):
 def access_questions():
     base_folders = ['Easy', 'Medium', 'Hard']
     process_folders('questions/', base_folders)
+
 
 access_questions()
